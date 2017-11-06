58efaac791b975b0403ba7e5a9f39f50a97dae2d
valentin messiaen <valentin.messiaen@gmail.com>
2017-11-06 16:18:52 +0100

    POST

--- app.js
+++ app.js
@@ -56,48 +56,61 @@ app.listen(app.get('port'), function() {
   console.log('Bot is running on port ', app.get('port'));
 });
 
-function sendTextMessage(sender, text) {
-    let data = { text:text }
-    let access_token = "EAATNZAO4UwDIBAAgvpZAbJwehQMJHPoZAL1PcUme00ct4Me0nbEuRaFdBCYUyfpQQQdgpWDjIsU1xzIRwMB8Xl4vKg91WFuqXRCsZCr94D8NdmjlS6Q6AOFLMvVpvq90xDLZAtyJZAONz47KCIhs7iZAZBieftZAUD83xEBbDYdrMtQZDZD";
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: access_token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: data,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
-
 app.post('/webhook/', function (req, res) {
+  console.log('SMMSMS ---> ', req);
     let message_events = req.body.entry[0].messaging
     for (message_event of message_events) {
         let sender = message_event.sender.id
         if (message_event.message && message_event.message.text) {
             let text = message_event.message.text
-            text = text || "";
-
-            let commande = text.split(' ');
-            switch(commande) {
-                case "!random":
-                    sendRandomImageMessage(sender);
-                break;
-
-                default:
-                    sendTextMessage(sender, "J'ai recu : " + text.substring(0, 200));
-            }
-
+            sendTextMessage(sender, "J'ai recu : " + text.substring(0, 200))
         }
     }
     res.sendStatus(200)
 })
+//
+// function sendTextMessage(sender, text) {
+//     let data = { text:text }
+//     let access_token = "EAATNZAO4UwDIBAAgvpZAbJwehQMJHPoZAL1PcUme00ct4Me0nbEuRaFdBCYUyfpQQQdgpWDjIsU1xzIRwMB8Xl4vKg91WFuqXRCsZCr94D8NdmjlS6Q6AOFLMvVpvq90xDLZAtyJZAONz47KCIhs7iZAZBieftZAUD83xEBbDYdrMtQZDZD";
+//     request({
+//         url: 'https://graph.facebook.com/v2.6/me/messages',
+//         qs: {access_token: access_token},
+//         method: 'POST',
+//         json: {
+//             recipient: {id:sender},
+//             message: data,
+//         }
+//     }, function(error, response, body) {
+//         if (error) {
+//             console.log('Error sending messages: ', error)
+//         } else if (response.body.error) {
+//             console.log('Error: ', response.body.error)
+//         }
+//     })
+// }
+//
+// app.post('/webhook/', function (req, res) {
+//     let message_events = req.body.entry[0].messaging
+//     for (message_event of message_events) {
+//         let sender = message_event.sender.id
+//         if (message_event.message && message_event.message.text) {
+//             let text = message_event.message.text
+//             text = text || "";
+//
+//             let commande = text.split(' ');
+//             switch(commande) {
+//                 case "!random":
+//                     sendRandomImageMessage(sender);
+//                 break;
+//
+//                 default:
+//                     sendTextMessage(sender, "J'ai recu : " + text.substring(0, 200));
+//             }
+//
+//         }
+//     }
+//     res.sendStatus(200)
+// })
 //BOT MESSENGER
 // let bot = new Bot({
 //    token: "EAATNZAO4UwDIBAAgvpZAbJwehQMJHPoZAL1PcUme00ct4Me0nbEuRaFdBCYUyfpQQQdgpWDjIsU1xzIRwMB8Xl4vKg91WFuqXRCsZCr94D8NdmjlS6Q6AOFLMvVpvq90xDLZAtyJZAONz47KCIhs7iZAZBieftZAUD83xEBbDYdrMtQZDZD",
