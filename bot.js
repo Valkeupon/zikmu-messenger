1471e61e336503b03bce32110cc6d8565e755085
valentin messiaen <valentin.messiaen@gmail.com>
2017-11-07 15:03:23 +0100

    split bot

--- bot.js
+++ bot.js
@@ -1,1 +1,51 @@
+const request = require('request');
+const config = require('config');
+const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
 
+let app = express();
+const VALIDATION_TOKEN = "8bQ9470R9we90Jo8q4TcS85vCJa0vqCrpUM8LMoO";
+
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+  }
+});
+
+app.post('/webhook/', function (req, res) {
+    let message_events = req.body.entry[0].messaging
+    for (message_event of message_events) {
+        let sender = message_event.sender.id
+        if (message_event.message && message_event.message.text) {
+            let text = message_event.message.text
+            sendTextMessage(sender, "J'ai recu : " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+});
+
+function sendTextMessage(sender, text) {
+    let data = { text:text }
+    let access_token = "EAAXkoGyQMgUBAMfLg5CAzB0zNFnlYPk9s4pUZCOZAED6Hq40O9mhqqWYFFfaOtiSv3PDbPnnejhZBy7ZAfv4ZAYBH6gpTKwmTPlj9VptMkZCHy4432dgDLNOD3itCoer8an8Qi2gKknjMqEvfIrAsKy5ieslVdoZAwdLHZC9cVDUxwZDZD";
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: access_token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: data,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
