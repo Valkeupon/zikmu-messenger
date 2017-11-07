a25d4f61be6bdfc0d27250b27997776e82116be5
valentin messiaen <valentin.messiaen@gmail.com>
2017-11-07 11:21:35 +0100

    add dashboard + Bot messenger

--- app.js
+++ app.js
@@ -85,137 +85,57 @@ function sendTextMessage(sender, text) {
     })
 }
 
-// app.listen(app.get('port'), function() {
-//   console.log('Bot is running on port ', app.get('port'));
-// });
+//uncomment after placing your favicon in /public
+app.use(logger('dev'));
+app.use(session({secret: 'ssshhhhh'}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bot.middleware());
+
+
+//Homepage
+app.use('/', routes);
+app.use('/admin', admin);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+console.log("ENV --->", app.get('env'));
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
 
 https.createServer({
         key: privateKey,
         cert: certificate,
         ca: ca
 }, app).listen(443);
-//
-// function sendTextMessage(sender, text) {
-//     let data = { text:text }
-//     let access_token = "EAATNZAO4UwDIBAAgvpZAbJwehQMJHPoZAL1PcUme00ct4Me0nbEuRaFdBCYUyfpQQQdgpWDjIsU1xzIRwMB8Xl4vKg91WFuqXRCsZCr94D8NdmjlS6Q6AOFLMvVpvq90xDLZAtyJZAONz47KCIhs7iZAZBieftZAUD83xEBbDYdrMtQZDZD";
-//     request({
-//         url: 'https://graph.facebook.com/v2.6/me/messages',
-//         qs: {access_token: access_token},
-//         method: 'POST',
-//         json: {
-//             recipient: {id:sender},
-//             message: data,
-//         }
-//     }, function(error, response, body) {
-//         if (error) {
-//             console.log('Error sending messages: ', error)
-//         } else if (response.body.error) {
-//             console.log('Error: ', response.body.error)
-//         }
-//     })
-// }
-//
-// app.post('/webhook/', function (req, res) {
-//     let message_events = req.body.entry[0].messaging
-//     for (message_event of message_events) {
-//         let sender = message_event.sender.id
-//         if (message_event.message && message_event.message.text) {
-//             let text = message_event.message.text
-//             text = text || "";
-//
-//             let commande = text.split(' ');
-//             switch(commande) {
-//                 case "!random":
-//                     sendRandomImageMessage(sender);
-//                 break;
-//
-//                 default:
-//                     sendTextMessage(sender, "J'ai recu : " + text.substring(0, 200));
-//             }
-//
-//         }
-//     }
-//     res.sendStatus(200)
-// })
-//BOT MESSENGER
-// let bot = new Bot({
-//    token: "EAATNZAO4UwDIBAAgvpZAbJwehQMJHPoZAL1PcUme00ct4Me0nbEuRaFdBCYUyfpQQQdgpWDjIsU1xzIRwMB8Xl4vKg91WFuqXRCsZCr94D8NdmjlS6Q6AOFLMvVpvq90xDLZAtyJZAONz47KCIhs7iZAZBieftZAUD83xEBbDYdrMtQZDZD",
-//    verify: "8bQ9470R9we90Jo8q4TcS85vCJa0vqCrpUM8LMoO"
-// });
-// //LOG ERROR
-// bot.on('error', function(err){
-//    console.log('BOT ERROR', err.message)
-// });
-//
-// bot.on('message', (payload, reply) => {
-//     let text = payload.message.text
-//     reply({
-//         text
-//     }, (err) => {
-//         if (err) {
-//             console.log('BOT ERR -->', err.message)
-//         }
-//
-//         console.log(`Echoed back : ${text}`)
-//     })
-// });
-
-//uncomment after placing your favicon in /public
-// app.use(logger('dev'));
-// app.use(session({secret: 'ssshhhhh'}));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.use(bot.middleware());
-//
-//
-// //Homepage
-// app.use('/', routes);
-// app.use('/admin', admin);
-//
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-//
-// const httpServer = http.createServer(bot.middleware());
-// httpServer.listen(8080);
-//
-// https.createServer({
-//         key: privateKey,
-//         cert: certificate,
-//         ca: ca
-// }, app).listen(443);
-//
-// // error handlers
-// console.log("ENV --->", app.get('env'));
-// // development error handler
-// // will print stacktrace
-// if (app.get('env') === 'development') {
-//   app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//       message: err.message,
-//       error: err
-//     });
-//   });
-// }
-//
-// // production error handler
-// // no stacktraces leaked to user
-// app.use(function(err, req, res, next) {
-//   res.status(err.status || 500);
-//   res.render('error', {
-//     message: err.message,
-//     error: {}
-//   });
-// });
-
-
-
 
 
 module.exports = app;
