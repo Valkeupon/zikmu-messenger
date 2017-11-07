50a00257281f54030822de260aa136a19177bc68
valentin messiaen <valentin.messiaen@gmail.com>
2017-11-07 14:37:29 +0100

    change

--- app.js
+++ app.js
@@ -13,6 +13,52 @@ const request = require('request');
 const config = require('config');
 //const Bot = require('messenger-bot');
 
+const VALIDATION_TOKEN = "8bQ9470R9we90Jo8q4TcS85vCJa0vqCrpUM8LMoO";
+
+app.get('/webhook', function(req, res) {
+  console.log(req.query);
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+  }
+});
+
+app.post('/webhook/', function (req, res) {
+    let message_events = req.body.entry[0].messaging
+    for (message_event of message_events) {
+        let sender = message_event.sender.id
+        if (message_event.message && message_event.message.text) {
+            let text = message_event.message.text
+            sendTextMessage(sender, "J'ai recu : " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+});
+
+sendTextMessage: (sender, text) => {
+  console.log('TEXT', text);
+    let data = { text:text }
+    let access_token = "EAAXkoGyQMgUBAEnsq7D3gZAqwQvodDZARJVOQv1ZBk5SIBwABy0KMpiQY2TCu9zJQyVfOZB6zjxZCT8vlVzZCBPwI2ZASkLpLVjRU4KDCcHEBUthWRZAZAvM9IOqPH78RgjKzysyh3PhsCrR4Yvaab8IhRDfjTCoVqEjbxQCPaRlG2AZDZD";
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: access_token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: data,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+};
+
 
 const routes = require('./routes/index');
 const admin = require('./routes/admin');
@@ -44,50 +90,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Homepage
 app.use('/', routes);
 app.use('/admin', admin);
-
-const VALIDATION_TOKEN = "8bQ9470R9we90Jo8q4TcS85vCJa0v";
-//"8bQ9470R9we90Jo8q4TcS85vCJa0vqCrpUM8LMoO"
-app.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === VALIDATION_TOKEN) {
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);
-  }
-});
-
-app.post('/webhook/', function (req, res) {
-    let message_events = req.body.entry[0].messaging
-    for (message_event of message_events) {
-        let sender = message_event.sender.id
-        if (message_event.message && message_event.message.text) {
-            let text = message_event.message.text
-            sendTextMessage(sender, "J'ai recu : " + text.substring(0, 200))
-        }
-    }
-    res.sendStatus(200)
-});
-
-sendTextMessage: (sender, text) => {
-    let data = { text:text }
-    let access_token = "EAAXkoGyQMgUBAEnsq7D3gZAqwQvodDZARJVOQv1ZBk5SIBwABy0KMpiQY2TCu9zJQyVfOZB6zjxZCT8vlVzZCBPwI2ZASkLpLVjRU4KDCcHEBUthWRZAZAvM9IOqPH78RgjKzysyh3PhsCrR4Yvaab8IhRDfjTCoVqEjbxQCPaRlG2AZDZD";
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: access_token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: data,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
